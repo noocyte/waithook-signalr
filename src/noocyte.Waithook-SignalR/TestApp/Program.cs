8dd41a78a4cdb3a9332c95a95a905b8705e01c04
Jarle Nygård <Jarle.Nygard@powel.no>
2019-12-12 13:26:59 +0100

    All working-ish

---
+++ src/noocyte.Waithook-SignalR/TestApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace TestApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            var baseUrl = "https://localhost:5001";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl($"{baseUrl}/waithookhub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.On<string>("Hook", s =>
+            {
+                var msg = JsonSerializer.Deserialize<WaithookMessage>(s);
+                Console.WriteLine(msg.Body);
+            });
+
+            await connection.StartAsync();
+
+            while (connection.State != HubConnectionState.Connected)
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+            var client = new HttpClient();
+            var model = new
+            {
+                id = "some id",
+                name = "jarle"
+            };
+            var content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync($"{baseUrl}/waithook/{connection.ConnectionId}", content);
+            Console.WriteLine(response.StatusCode);
+            Console.ReadLine();
+
+        }
+    }
+
+    public class WaithookMessage
+    {
+        public string Method { get; set; }
+        public string Url { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ src/noocyte.Waithook-SignalR/noocyte.Waithook-SignalR.Web/Controllers/WaithookController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace noocyte.Waithook_SignalR.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WaithookController : ControllerBase
+    {
+        private readonly IHubContext<WaithookHub> _hubContext;
+
+        public WaithookController(IHubContext<WaithookHub> hubContext)
+        {
+            _hubContext = hubContext ?? throw new ArgumentNullException(nameof(hubContext));
+        }
+
+        [HttpPost("{unique_name}")]
+        public async Task<IActionResult> PostIt(string unique_name, CancellationToken cancellationToken)
+        {
+            if (unique_name is null) throw new ArgumentNullException(nameof(unique_name));
+
+            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                var body = await reader.ReadToEndAsync();
+                var message = new WaithookMessage(Request, body);
+                var content = JsonSerializer.Serialize(message);
+                await _hubContext.Clients.Client(unique_name).SendAsync("Hook", content, cancellationToken);
+                return Ok();
+            }
+        }
+    }
+
+    public class WaithookHub : Hub
+    {
+    }
+
+    public class WaithookMessage
+    {
+        public WaithookMessage(HttpRequest request, string body)
+        {
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            Body = body;
+            Method = request.Method;
+            Url = request.Path;
+            Headers = request.Headers.ToDictionary(k => k.Key, v => v.Value.ToString());
+        }
+
+        public string Method { get; }
+        public string Url { get; }
+        public Dictionary<string, string> Headers { get; }
+        public string Body { get; }
+    }
+}
